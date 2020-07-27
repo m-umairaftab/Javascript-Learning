64192792614060d90e0cf78756051ffe932e9aad
m-umairaftab <m.umair.umairaftab@gmail.com>
2020-07-27 12:34:19 +0500

    Arithmetic Operator example

---
+++ javascript/arithmetic-operator.js
@@ -0,0 +1,14 @@
+var a=4
+var b=2
+var c=a+b
+var d=a-b
+var e=a*b
+var f=a/b
+var g=a%b
+var h=a**b
+console.log(`additon of ${a} and ${b} is ${c}`)
+console.log(`subtraction of ${a} and ${b} is ${d}`)
+console.log(`multiplication of ${a} and ${b} is ${e}`)
+console.log(`division of ${a} and ${b} is ${f}`)
+console.log(`modulus of ${a} and ${b} is ${g}`)
+console.log(`exponent of ${a} and ${b} is ${h}`)
