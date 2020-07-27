281e0e44d598b884537fa4c10ea784287a80e8bc
m-umairaftab <m.umair.umairaftab@gmail.com>
2020-07-27 14:38:27 +0500

    Increment and Decrement example

---
+++ javascript/increment-decrement.js
@@ -0,0 +1,16 @@
+var a=6
+var b=++a
+var c=6
+var d=c++
+var e=6
+var f=--e
+var g=6
+var h=g--
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
+console.log(h)
