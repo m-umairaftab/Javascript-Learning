cc96a78edcae8a220ad55d45fecc3787614f43bf
m-umairaftab <m.umair.umairaftab@gmail.com>
2020-07-28 13:10:35 +0500

    Comparsion Operator example

---
+++ javascript/comparsion-operator.js
@@ -0,0 +1,12 @@
+var a=4
+var b=5
+var c="4"
+console.log(a==c)
+console.log(a===c)
+console.log(a==b)
+console.log(a===b)
+console.log(a>=b)
+console.log(a<=b)
+console.log(a>b)
+console.log(a<b)
+console.log(a!==b)
