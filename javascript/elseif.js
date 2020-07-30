360f234e8e4e6e1f1831852b48dbf0ae88e8d4cd
m-umairaftab <m.umair.umairaftab@gmail.com>
2020-07-30 16:11:22 +0500

    Else If example

---
+++ javascript/elseif.js
@@ -0,0 +1,30 @@
+var score=prompt("what is your score")
+score=Number(score)
+if (score >= 80)
+{
+    console.log("Grade =A+")
+}
+else if (score >= 70)
+{
+    console.log("Grade =A")
+}
+else if (score >= 60)
+{
+    console.log("Grade =B")
+}
+else if (score >= 50)
+{
+    console.log("Grade =C")
+}
+else if (score >= 40)
+{
+    console.log("Grade =D")
+}
+else if (score >= 33)
+{
+    console.log("Grade =E")
+}
+else 
+{
+    console.log("Failed")
+}
