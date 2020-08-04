b1cc66f43c4d678ab392533cdb73b99af5b0313d
m-umairaftab <m.umair.umairaftab@gmail.com>
2020-08-04 11:03:52 +0500

    Update Array Element Using Array

---
+++ javascript/update-array-element-using-index.js
@@ -0,0 +1,10 @@
+var foods=[]
+console.log(foods)
+foods[0]="pizza"
+foods[1]="Burger"
+foods[2]="snacks"
+console.log(foods)
+console.log(foods[1])
+foods[1]="sandwich"
+console.log(foods)
+console.log(foods[1])
