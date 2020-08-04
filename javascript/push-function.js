adf8ffb218f976d7bc4097dfe2bfe93bcdae8c19
m-umairaftab <m.umair.umairaftab@gmail.com>
2020-08-04 11:24:21 +0500

    Push Function example

---
+++ javascript/push-function.js
@@ -0,0 +1,8 @@
+var foods=[]
+foods.push(prompt("enter first food item"))
+foods.push(prompt("enter second food item"))
+foods.push(prompt("enter third food item"))
+console.log(foods)
+console.log(foods[0])
+console.log(foods[1])
+console.log(foods[2])
