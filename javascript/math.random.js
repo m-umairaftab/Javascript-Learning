61457a9b987c947a4757cf345468727435e54825
m-umairaftab <m.umair.umairaftab@gmail.com>
2020-08-06 16:47:24 +0500

    Math.random example

---
+++ javascript/math.random.js
@@ -0,0 +1,8 @@
+var a=Math.random()
+console.log("A = "+a)
+var num1=a*6
+console.log("Num1 = "+num1)
+var num2=num1+1
+console.log("Num2 = "+num2)
+var num3=Math.floor(num2)
+console.log("Num3 = "+num3)
