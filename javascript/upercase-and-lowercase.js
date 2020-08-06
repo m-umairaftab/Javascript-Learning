3fbf3d767e174c83e2fb1ff7b8177f3d90e5b9e1
m-umairaftab <m.umair.umairaftab@gmail.com>
2020-08-06 12:42:10 +0500

    Upercase and Lowercase example

---
+++ javascript/upercase-and-lowercase.js
@@ -0,0 +1,10 @@
+var food="Pizza"
+var input="pizza"
+console.log(food.toLowerCase())
+console.log(food.toUpperCase())
+if(food.toLowerCase()===input.toLowerCase()){
+    console.log("match")
+}
+else{
+    console.log("Not match")
+}
