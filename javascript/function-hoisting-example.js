a17d5a4656d6b5b722f08d1f5844c21254016641
m-umairaftab <m.umair.umairaftab@gmail.com>
2020-08-10 12:53:25 +0500

    Function Hoisting example

---
+++ javascript/function-hoisting-example.js
@@ -0,0 +1,10 @@
+var mul=multiply(3,2)
+console.log("Mul = "+ mul)
+var add=sum(4,5)
+console.log("Add = "+ add)
+function sum(a,b){
+    return a+b
+}
+function multiply(num1,num2){
+    return num1*num2
+}
