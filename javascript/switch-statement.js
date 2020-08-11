17b86f6395ccdf2050781630f8b221b2b034c36a
m-umairaftab <m.umair.umairaftab@gmail.com>
2020-08-11 10:18:58 +0500

    Switch Statement example

---
+++ javascript/switch-statement.js
@@ -0,0 +1,11 @@
+var day = 3
+switch(day){
+    case 0:
+        console.log("Today is Sunday")
+        break
+    case 6:
+        console.log("Today is Saturday")
+        break
+    default:
+        console.log("Looking forward to the Weekend")
+}
