393dafc06c228c4a9cf18087851c1b745177bc2d
m-umairaftab <m.umair.umairaftab@gmail.com>
2020-08-11 11:00:29 +0500

    Switch Statement example

---
+++ javascript/switch-example.js
@@ -0,0 +1,21 @@
+var  a=prompt("please enter character : ")
+switch(a){
+    case "A":
+        console.log("A is vowel")
+        break
+    case "E":
+        console.log("E is vowel")
+        break
+    case "I":
+        console.log("I is vowel")
+        break
+    case "O":
+        console.log("O is vowel")
+        break 
+    case "U":
+        console.log("U is vowel")
+     break
+    default:
+        console.log("Not vowel")
+        break
+}
