347688f900b316ba5dd699f6ffc5c78a6761445a
m-umairaftab <m.umair.umairaftab@gmail.com>
2020-08-13 10:21:31 +0500

    Split example

--- javascript/split.js
+++ javascript/split.js
@@ -1,9 +1,9 @@
-var a="To be or Not to be"
-var b=a.split(" ")
-console.log(b)
-var c="To, be, or, Not to, be"
-var d=c.split(",")
-console.log(d)
-var e="To be| or Not |to be"
-var f=e.split("|")
-console.log(f)
+var a="To be or Not to be";
+var b=a.split(" ");
+console.log(b);
+var c="To, be, or, Not to, be";
+var d=c.split(",");
+console.log(d);
+var e="To be| or Not |to be";
+var f=e.split("|");
+console.log(f);
