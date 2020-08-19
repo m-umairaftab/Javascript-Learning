70af7485050a03c5664bef39d9d854667a6838be
m-umairaftab <m.umair.umairaftab@gmail.com>
2020-08-19 22:42:12 +0500

    Declaration and Initialization example

---
+++ javascript/Declaration-and-Initialization.js
@@ -0,0 +1,31 @@
+// Declare firstVar:
+let firstVar; 
+
+// Initialize firstVar:
+firstVar = 1; 
+
+// Declare and Initialize secondVar:
+const secondVar = "String"; 
+
+// Declare thirdVar and fourthVar:
+let thirdVar, fourthVar; 
+// Initialize thirdVar:
+thirdVar = true; 
+
+// Initialize fourthVar:
+fourthVar = null; 
+
+// Declare and Initialize fifthVar and sixthVar:
+const fifthVar = 1.01, 
+    sixthVar = "Sixth"; 
+
+// Declare seventhVar:
+let seventhVar;
+
+console.log(firstVar);
+console.log(secondVar);
+console.log(thirdVar);
+console.log(fourthVar);
+console.log(fifthVar);
+console.log(sixthVar);
+console.log(seventhVar);
