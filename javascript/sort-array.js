373c25134cef3049c2a6821ee56059e789077757
m-umairaftab <m.umair.umairaftab@gmail.com>
2020-08-24 10:31:22 +0500

    Sort array example

---
+++ javascript/sort-array.js
@@ -0,0 +1,9 @@
+var a = ['c', 'a', 'd', 'b', 'aa'];
+var b = [9, 2, 13, 7, 1, 12, 123];
+
+// Sort in ascending lexicographical order using a built-in
+a.sort();
+b.sort();
+
+console.log('a:', a);
+console.log('b:', b);
