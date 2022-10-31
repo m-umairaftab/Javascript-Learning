f7068ee97b412046424760877918da3d7cc35965
Umair Aftab <m.umair.umairaftab@gmail.com>
2022-10-31 15:53:42 +0500

    Object

---
+++ Node_Learning/object.js
@@ -0,0 +1,21 @@
+const people = {
+    friends : []
+};
+const friend1={
+    firstName : "zgm",
+    lastName : "javed",
+    id : 12345
+};
+const friend2={
+    firstName : "Umair",
+    lastName : "Aftab",
+    id : 12345
+};
+const friend3={
+    firstName : "ali",
+    lastName : "ahmad",
+    id : 12345
+};
+console.log(people.friends=[friend1.firstName, friend1.lastName, friend1.id])
+console.log(people.friends=[friend2.firstName, friend2.lastName, friend2.id])
+console.log(people.friends=[friend3.firstName, friend3.lastName, friend3.id])
