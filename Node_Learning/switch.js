283382d6e2cd05e276d53ae9100f06fb6a6cdb4a
Umair Aftab <m.umair.umairaftab@gmail.com>
2022-11-01 11:32:13 +0500

    Switch Statement

---
+++ Node_Learning/switch.js
@@ -0,0 +1,21 @@
+const activity='Get up';
+switch(activity) {
+    case "Get up":
+        console.log("It is 6:30AM");
+        break;
+    case "Breakfast":
+        console.log("It is 7:00AM");
+        break;
+    case "Drive to work":
+        console.log("It is 8:00AM");
+        break;
+    case "Lunch":
+        console.log("It is 12:00PM");
+        break;
+    case "Drive home":
+        console.log("It is 5:00PM");
+        break;
+    case "Dinner":
+        console.log("It is 6:30PM");
+        break;
+}
