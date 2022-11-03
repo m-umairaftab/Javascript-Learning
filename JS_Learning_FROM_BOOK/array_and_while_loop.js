c3e9b712ae4d67d9c03647408c65770e510683c1
Umair Aftab <m.umair.umairaftab@gmail.com>
2022-11-03 10:22:29 +0500

    Array And While Loop

---
+++ JS_Learning_FROM_BOOK/array_and_while_loop.js
@@ -0,0 +1,11 @@
+const someArray = ["Mike", "Antal", "Marc", "Emir", "Louiza", "Jacky"]
+let notFound = true
+while (notFound && someArray.length > 0) {
+    if (someArray[0] === "Louiza") {
+        console.log("Found her!")
+        notFound = false
+    } 
+    else {
+        someArray.shift()
+    }
+}
