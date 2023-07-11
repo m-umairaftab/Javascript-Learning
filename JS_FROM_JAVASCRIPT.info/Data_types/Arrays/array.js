cbfc1f8413fc1f6ca6c0fb829c9e3a2e7f9ea19a
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-07-11 10:10:50 +0500

    Array

---
+++ JS_FROM_JAVASCRIPT.info/Data_types/Arrays/array.js
@@ -0,0 +1,7 @@
+let fruits = ['apple', 'orange', 'mango', 'banana'];
+console.log('Full Array : ', fruits);
+console.log('Length of Array : ', fruits.length);
+console.log('2nd element of array : ', fruits[1]);
+// Updating the 2nd element of array
+fruits[1] = 'peach';
+console.log('After updating the 2nd element  : ', fruits[1]);
