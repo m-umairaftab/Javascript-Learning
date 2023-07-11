458ca47dcc7ce919b5b9200d28e741b2c316d6ab
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-07-11 10:47:46 +0500

    Push and Pop method

---
+++ JS_FROM_JAVASCRIPT.info/Data_types/Arrays/push_pop_method.js
@@ -0,0 +1,6 @@
+let arr = ['apple', 'mango', 'orange',];
+console.log(arr);
+arr.pop() // remove orange.. pop method removes element from end of any array
+console.log(arr);
+arr.push('peach'); // add peach at index 2..push method add element of any array
+console.log(arr);
