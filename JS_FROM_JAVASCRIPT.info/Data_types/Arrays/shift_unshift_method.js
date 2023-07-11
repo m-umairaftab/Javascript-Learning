1ff412a59ed43f238bc2ec66a48c42dd7df852f2
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-07-11 10:50:13 +0500

    Shift and Unshift method

---
+++ JS_FROM_JAVASCRIPT.info/Data_types/Arrays/shift_unshift_method.js
@@ -0,0 +1,6 @@
+let arr = ['apple', 'mango', 'orange',];
+console.log(arr);
+arr.shift() // remove apple.. pop method removes element from start of any array
+console.log(arr);
+arr.unshift('peach'); // add peach at index 0..push method add element from start of any array
+console.log(arr);
