8da56ee1748c8c3a708c21595c376f57389a1347
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-09-11 12:41:54 +0500

    Object. fromEntries

---
+++ JS_FROM_JAVASCRIPT.info/Data_types/Map and SET/object.fromEntries.js
@@ -0,0 +1,9 @@
+let prices = Object.fromEntries([
+    ['banana', 1],
+    ['orange', 2],
+    ['meat', 4]
+]);
+
+// now prices = { banana: 1, orange: 2, meat: 4 }
+
+console.log(prices.orange); // 2
