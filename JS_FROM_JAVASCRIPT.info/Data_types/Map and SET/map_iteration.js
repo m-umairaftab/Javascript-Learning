d876198458e05ea2e5cd809ef8c36633d07dbaee
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-09-11 12:27:59 +0500

    Iteration over map

--- JS_FROM_JAVASCRIPT.info/Data_types/Map and SET/map_iteration.js
+++ JS_FROM_JAVASCRIPT.info/Data_types/Map and SET/map_iteration.js
@@ -1,23 +1,23 @@
 let recipeMap = new Map([
     ['cucumber', 500],
     ['tomatoes', 350],
-    ['onion',    50]
-  ]);
-  
-  // iterate over keys (vegetables)
-  console.log('Iteration over keys');
-  for (let vegetable of recipeMap.keys()) {
+    ['onion', 50]
+]);
+
+// iterate over keys (vegetables)
+console.log('Iteration over keys');
+for (let vegetable of recipeMap.keys()) {
     console.log(vegetable); // cucumber, tomatoes, onion
-  }
-  
-  // iterate over values (amounts)
-  console.log('Iteration over values');
-  for (let amount of recipeMap.values()) {
+}
+
+// iterate over values (amounts)
+console.log('Iteration over values');
+for (let amount of recipeMap.values()) {
     console.log(amount); // 500, 350, 50
-  }
-  
-  // iterate over [key, value] entries
-  console.log(`Iteration over entries`);
-  for (let entry of recipeMap) { // the same as of recipeMap.entries()
+}
+
+// iterate over [key, value] entries
+console.log(`Iteration over entries`);
+for (let entry of recipeMap) { // the same as of recipeMap.entries()
     console.log(entry); // cucumber,500 (and so on)
-  }
+}
