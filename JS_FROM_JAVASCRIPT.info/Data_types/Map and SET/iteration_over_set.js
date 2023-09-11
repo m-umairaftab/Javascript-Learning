88debddaa87f589fc8a170ded4cbabd35f118eec
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-09-11 15:21:31 +0500

    Iteration Over Set

---
+++ JS_FROM_JAVASCRIPT.info/Data_types/Map and SET/iteration_over_set.js
@@ -0,0 +1,8 @@
+let set = new Set(["oranges", "apples", "bananas"]);
+
+for (let value of set) console.log(value);
+
+// the same with forEach:
+set.forEach((value, valueAgain, set) => {
+  console.log(value);
+});
